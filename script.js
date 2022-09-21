5ebb0e15f9f96d57a7d244452fe24dedc5a8e627
LexiestLeszek <leszekmel@gmail.com>
2022-09-21 14:57:01 +0200

    updated index html and added const to script

--- script.js
+++ script.js
@@ -42,3 +42,19 @@ function getRandomChoice() {
 function isGameOver() {
   return playerScore === 5 || computerScore === 5
 }
+
+// UI
+
+const scoreInfo = document.getElementById('scoreInfo')
+const scoreMessage = document.getElementById('scoreMessage')
+const playerScorePara = document.getElementById('playerScore')
+const computerScorePara = document.getElementById('computerScore')
+const playerSign = document.getElementById('playerSign')
+const computerSign = document.getElementById('computerSign')
+const rockBtn = document.getElementById('rockBtn')
+const paperBtn = document.getElementById('paperBtn')
+const scissorsBtn = document.getElementById('scissorsBtn')
+const endgameModal = document.getElementById('endgameModal')
+const endgameMsg = document.getElementById('endgameMsg')
+const overlay = document.getElementById('overlay')
+const restartBtn = document.getElementById('restartBtn')
